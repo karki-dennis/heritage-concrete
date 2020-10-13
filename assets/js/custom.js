0d0ae66250e9b7ad4dddb9088e01f4844f0915e1
karki-dennis <karki.dennis@gmail.com>
2020-10-13 23:33:20 +0545

    Repo Init

---
+++ assets/js/custom.js
@@ -0,0 +1,21 @@
+"use strict";
+
+jQuery(document).ready(function ($) {
+  $("#primary-menu").find(".menu-item-has-children > a").append('<i class="arrow"></i>');
+  $(".menu-item-has-children .arrow").on("click", function (i) {
+    i.preventDefault();
+    $(this).toggleClass("open");
+    $(this).parent().parent().children("ul.sub-menu").slideToggle();
+  });
+  $(".hamburger").click(function () {
+    $(this).toggleClass("is-active");
+    $(".off-canvas-wrapper").toggleClass("off-canvas-wrapper--active");
+  }); //banner slider
+
+  $(".home-banner__slider").slick({
+    dots: false,
+    arrows: false
+    /*autoplay: true,*/
+
+  });
+});
